8bff4181cce72607e73489e664b0ecd5787c10f8
Won Do-Youn <wondy1128@naver.com>
2017-05-04 14:25:01 +0900

    variables 작성중... testcode미작성

--- modelHelper/variables.py
+++ modelHelper/variables.py
@@ -9,8 +9,28 @@ def variable_xavier(name,shape):
         tf.variable
     """
     var = tf.get_variable(name = name, shape = shape , initializer=tf.contrib.layers.xavier_initializer())
-    variable_summaries(var)
+    variable_summaries(name=name, var=var)
     return var
+
+def variable_random(name, shape, stddev = 0.01, mean = 0.0):
+    """
+    변수를 random_norm으로 초기화해서 반환
+    :param name:
+        사용 할 variable의 이름
+    :param shape:
+        사용 할 variable의 shape, dim
+    :param stddev:
+        default = 0.1
+         random_norm에서 사용될 표준편차
+    :param mean:
+        default = 0.0
+        random_norm 에서 사용될 평균
+    :return:
+        tf.variable
+    """
+    var = tf.random_normal(shape = shape, mean = mean , stddev = stddev , name = name)
+    variable_summaries(name=name, var=var)
+    return tf.Variable(var)
 
 def variable_truncated(name, shape, stddev=0.1):
     """
@@ -26,23 +46,24 @@ def variable_truncated(name, shape, stddev=0.1):
         tf.variable
     """
     var = tf.truncated_normal(name = name, shape = shape, stddev = stddev)
-    variable_summaries(var)
-    return var
+    variable_summaries(name = name, var = var)
+    return tf.Variable(var)
 
-def variable_summaries(var):
+def variable_summaries(name, var):
     """
     tf.variable의 tensorboard에서 볼 수 있도록 정리
     Ex) average, stddev, max, min, histogram.....
+    :param name:
     :param var:
         tensorboard에 표시 할 변수
     """
     with tf.name_scope('summary'):
         mean = tf.reduce_mean(var)
-        tf.summary.scalar('mean', mean)
-        tf.summary.scalar('stddev', tf.sqrt(tf.reduce_mean(tf.square(var - mean))))
-        tf.summary.scalar('max', tf.reduce_max(var))
-        tf.summary.scalar('min', tf.reduce_min(var))
-        tf.summary.histogram(var)
+        tf.summary.scalar(name+'/mean', mean)
+        tf.summary.scalar(name+'/stddev', tf.sqrt(tf.reduce_mean(tf.square(var - mean))))
+        tf.summary.scalar(name+'/max', tf.reduce_max(var))
+        tf.summary.scalar(name+'/min', tf.reduce_min(var))
+        tf.summary.histogram(name+'/variable',var)
 
 
 if __name__=="__main__":
