5dd74e9ca0610d5320f21c4b8b08ef95b43f4abd
koohanmo <rngksah@naver.com>
2017-05-09 21:13:02 +0900

    SRGAN 시작

---
+++ models/SRGAN/SRGAN.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+import modelHelper as mh
+
+
+class SRGAN(object):
+    def __init__(self):
+        pass
+
+    def inference(self):
+        pass
+
+    def generator(self):
+        pass
+
+    def discriminator(self):
+        pass
+
+
+
+
+
+
+
+if __name__=="__main__":
+    print ('------SRGAN------')
