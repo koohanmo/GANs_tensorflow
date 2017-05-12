dc7df0390eeabf92aa711d09ab13a3fbe8be2d8c
sanghyub <sanghyub91@gmail.com>
2017-05-12 21:56:52 +0900

    DCGAN model

---
+++ models/DCGAN/DC_Utils.py
@@ -0,0 +1,89 @@
+import numpy as np
+import scipy.misc
+import tensorflow as tf
+
+def save_images(images, size, image_path):
+    return imsave(inverse_transform(images), size, image_path)
+
+def inverse_transform(images):
+    return (images+1.)/2.
+
+def imsave(images, size, path):
+  image = np.squeeze(merge(images, size))
+  return scipy.misc.imsave(path, image)
+
+
+def merge(images, size):
+  h, w = images.shape[1], images.shape[2]
+  if (images.shape[3] in (3,4)):
+    c = images.shape[3]
+    img = np.zeros((h * size[0], w * size[1], c))
+    for idx, image in enumerate(images):
+      i = idx % size[1]
+      j = idx // size[1]
+      img[j * h:j * h + h, i * w:i * w + w, :] = image
+    return img
+  elif images.shape[3]==1:
+    img = np.zeros((h * size[0], w * size[1]))
+    for idx, image in enumerate(images):
+      i = idx % size[1]
+      j = idx // size[1]
+      img[j * h:j * h + h, i * w:i * w + w] = image[:,:,0]
+    return img
+  else:
+    raise ValueError('in merge(images,size) images parameter '
+                     'must have dimensions: HxW or HxWx3 or HxWx4')
+
+def linear(input_, output_size, scope=None, stddev=0.02, bias_start=0.0, with_w=False):
+      shape = input_.get_shape().as_list()
+
+      with tf.variable_scope(scope or "Linear"):
+          matrix = tf.get_variable("Matrix", [shape[1], output_size], tf.float32,
+                                   tf.random_normal_initializer(stddev=stddev))
+          bias = tf.get_variable("bias", [output_size],
+                                 initializer=tf.constant_initializer(bias_start))
+          if with_w:
+              return tf.matmul(input_, matrix) + bias, matrix, bias
+          else:
+              return tf.matmul(input_, matrix) + bias
+
+
+def conv_cond_concat(x, y):
+  """Concatenate conditioning vector on feature map axis."""
+  x_shapes = x.get_shape()
+  y_shapes = y.get_shape()
+  return concat([
+    x, y*tf.ones([x_shapes[0], x_shapes[1], x_shapes[2], y_shapes[3]])], 3)
+
+if "concat_v2" in dir(tf):
+  def concat(tensors, axis, *args, **kwargs):
+    return tf.concat_v2(tensors, axis, *args, **kwargs)
+else:
+  def concat(tensors, axis, *args, **kwargs):
+    return tf.concat(tensors, axis, *args, **kwargs)
+
+
+def deconv2d(input_, output_shape,
+             k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
+             name="deconv2d", with_w=False):
+    with tf.variable_scope(name):
+        # filter : [height, width, output_channels, in_channels]
+        w = tf.get_variable('w', [k_h, k_w, output_shape[-1], input_.get_shape()[-1]],
+                            initializer=tf.random_normal_initializer(stddev=stddev))
+
+        try:
+            deconv = tf.nn.conv2d_transpose(input_, w, output_shape=output_shape,
+                                            strides=[1, d_h, d_w, 1])
+
+        # Support for verisons of TensorFlow before 0.7.0
+        except AttributeError:
+            deconv = tf.nn.deconv2d(input_, w, output_shape=output_shape,
+                                    strides=[1, d_h, d_w, 1])
+
+        biases = tf.get_variable('biases', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
+        deconv = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
+
+        if with_w:
+            return deconv, w, biases
+        else:
+            return deconv
