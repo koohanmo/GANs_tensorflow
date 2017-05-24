7596a82db87927560f82777ab48713699fb0dc57
sanghyub <sanghyub91@gmail.com>
2017-05-24 23:07:47 +0900

    vgg19 MNIST test

--- models/SRGAN/vgg19.py
+++ models/SRGAN/vgg19.py
@@ -2,12 +2,12 @@ import tensorflow as tf
 from modelHelper import layers
 
 class VGG19:
-    def __init__(self, input, t, is_training):
+    def __init__(self, input, t):
         if input is None: return
-        self.out, self.phi = self.build_model(input, is_training)
+        self.out, self.phi = self.build_model(input)
         self.loss = self.inference_loss(self.out, t)
 
-    def build_model(self, t_input, is_training, reuse=False):
+    def build_model(self, t_input, reuse=False):
 
         with tf.variable_scope('vgg19', reuse=reuse):
             phi = []
@@ -15,11 +15,9 @@ class VGG19:
                 conv1a = layers.conv2d(
                                     layerName='vgg19_conv1a',
                                     t_input=t_input,
-                                    ksize= [3, 3, 3, 64],
+                                    ksize= [3, 3, 1, 64], #<--[3,3,3,64]
                                     strides=[1,1,1,1],
                                     padding='SAME')
-                conv1a = layers.batch_norm(conv1a, is_training)
-
                 BN = layers.batch_norm()
                 conv1a = BN(conv1a)
                 conv1a = layers.lrelu(conv1a)
@@ -39,8 +37,10 @@ class VGG19:
             pool_first = layers.maxPool(
                                         layerName='pool_first',
                                         t_input=conv1b,
-                                        ksize=2,
-                                        strides=2)
+                                        ksize=[1,2,2,1],
+                                        strides=[1,2,2,1],
+                                        padding='SAME'
+                                        )
             # pool 1
 
             with tf.variable_scope('conv2a'):
@@ -66,8 +66,13 @@ class VGG19:
                 conv2b = layers.lrelu(conv2b)
             phi.append(conv2b)
 
-            pool_second= layers.maxPool(conv2b, 2, 2)
-
+            pool_second= layers.maxPool(
+                                        layerName='pool_first',
+                                        t_input=conv2b,
+                                        ksize=[1, 2, 2, 1],
+                                        strides=[1, 2, 2, 1],
+                                        padding='SAME'
+                                        )
             # pool 2
 
             with tf.variable_scope('conv3a'):
@@ -114,8 +119,13 @@ class VGG19:
                 conv3d = layers.lrelu(conv3d)
             phi.append(conv3d)
 
-            pool_third = layers.maxPool(conv3d, 2, 2)
-
+            pool_third =layers.maxPool(
+                                        layerName='pool_first',
+                                        t_input=conv3d,
+                                        ksize=[1, 2, 2, 1],
+                                        strides=[1, 2, 2, 1],
+                                        padding='SAME'
+                                        )
 
             with tf.variable_scope('conv4a'):
                 conv4a = layers.conv2d(layerName='vgg19_conv4a',
@@ -161,7 +171,13 @@ class VGG19:
 
             phi.append(conv4d)
 
-            pool_fourth = layers.maxPool(conv4d, 2, 2)
+            pool_fourth = layers.maxPool(
+                                        layerName='pool_first',
+                                        t_input=conv4d,
+                                        ksize=[1, 2, 2, 1],
+                                        strides=[1, 2, 2, 1],
+                                        padding='SAME'
+                                        )
 
 
 
@@ -207,24 +223,112 @@ class VGG19:
 
             phi.append(conv5d)
 
-            pool_fifth = layers.maxPool(conv5d, 2, 2)
+            pool_fifth = layers.maxPool(
+                                        layerName='pool_first',
+                                        t_input=conv5d,
+                                        ksize=[1, 2, 2, 1],
+                                        strides=[1, 2, 2, 1],
+                                        padding='SAME'
+                                        )
 
-            flatten = layers.flatten(t_input=pool_fifth,flatDim=1)
+            flatten = layers.flatten(t_input=pool_fifth, flatDim=512)
 
             with tf.variable_scope('fc1'):
-                fc1 = layers.fullyConnected(flatten, 4096)
+                fc1 = layers.fullyConnected(flatten, [512, 4096], layerName='fc1')
                 fc1 = layers.lrelu(fc1)
             with tf.variable_scope('fc2'):
-                fc2 = layers.fullyConnected(fc1, 4096)
+                fc2 = layers.fullyConnected(fc1, [4096, 4096], layerName='fc2')
                 fc2 = layers.lrelu(fc2)
             with tf.variable_scope('softmax'):
-                fc3 = layers.fullyConnected(fc2, 100)
-
+                fc3 = layers.fullyConnected(fc2, [4096, 10], layerName='fc3')
             return fc3, phi
 
 
     def inference_loss(self, out, t):
         cross_entropy = tf.nn.softmax_cross_entropy_with_logits(
-            labels=tf.one_hot(t, 100),
+            labels=t,
+            #labels=tf.one_hot(t, 10), <-원래이거씀
             logits=out)
         return tf.reduce_mean(cross_entropy)
+
+
+if __name__=="__main__":
+       from tensorflow.examples.tutorials.mnist import input_data
+       mnist = input_data.read_data_sets('MNIST_data', one_hot = True)
+
+       X_data = tf.placeholder(tf.float32, shape = [None, 28*28])
+       X_image = tf.reshape(X_data, [-1, 28, 28, 1])
+       Y_label = tf.placeholder(tf.float32, shape = [None, 10])
+       tf.summary.image('input', X_image, 12)
+       TRIAN_LOG_DIR = './logs/train'
+       TEST_LOG_DIR = './logs/test'
+       CHECK_POINT_DIR = './checkpoint'
+
+       vgg= VGG19(None,None)
+       logits, _=vgg.build_model(X_image, False)
+
+       cost = vgg.inference_loss(out=logits, t=Y_label)
+       train =layers.trainOptimizer(cost = cost)
+
+       with tf.name_scope('accuracy'):
+           prediction = tf.equal(tf.argmax(logits, 1) , tf.argmax(Y_label, 1))
+           accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))
+           tf.summary.scalar(name = 'accuracy', tensor = accuracy)
+
+       merged = tf.summary.merge_all()
+
+       with tf.Session() as sess:
+           # last_epoch
+           # 복귀시킬 epoch 를 선언.
+           # tf.전역변수초기화하기 전 선언시켜둬야함.
+           last_epoch = tf.Variable(0, name='last_epoch')
+
+           sess.run(tf.global_variables_initializer())
+           writer_test = tf.summary.FileWriter(logdir = TEST_LOG_DIR, graph = sess.graph)
+           writer_train = tf.summary.FileWriter(logdir = TRIAN_LOG_DIR, graph = sess.graph)
+           # tf.summary.FileWriter(logdir = DIRECTORY , graph = sess.graph)
+           # 또는
+           # tf.summary.FileWriter(logdir = DIRECTORY)
+           # tf.summary.add_graph(sess.graph)
+
+           saver = layers.restoreSaver(session = sess,
+                               check_dir = CHECK_POINT_DIR)
+           start_from = sess.run(last_epoch)
+           epoch_run = 1000
+           batch_size = 100
+
+           total_batch = int(mnist.train.num_examples / batch_size)
+           for epoch in range(start_from, epoch_run):
+               for step in range(total_batch):
+                   print("epoch:",epoch," step:",step)
+                   batch = mnist.train.next_batch(batch_size=batch_size)
+                   sess.run(train, feed_dict={X_data:batch[0],
+                                              Y_label:batch[1]})
+
+               # Exhaused Memory Error
+               # batch size modified
+               # test data(10000) -> 분할해야함.
+               # train data - 분할
+
+               import random
+               random_batch = random.randint(0, len(mnist.test.images)-1000)
+
+               summary, acc = sess.run([merged, accuracy], feed_dict={X_data: mnist.test.images[random_batch:random_batch+1000],
+                                                           Y_label: mnist.test.labels[random_batch:random_batch+1000]})
+               print('===EPOCH %4s===' % (epoch))
+               writer_test.add_summary(summary, global_step=epoch)
+               print("[Test]Accuracy at epoch %s: %s" % (epoch, acc))
+
+               summary, acc = sess.run([merged, accuracy],
+                                       feed_dict={X_data: batch[0],
+                                                  Y_label: batch[1]})
+               writer_train.add_summary(summary, global_step=epoch)
+               print("[Training]Accuracy at epoch %s: %s" % (epoch, acc))
+
+               saver = layers.saveSaver(session=sess,
+                                 last_epoch=last_epoch,
+                                 global_step=epoch,
+                                 saver=saver,
+                                 check_dir=CHECK_POINT_DIR,
+                                 model_name='test_model'
+                                 )
